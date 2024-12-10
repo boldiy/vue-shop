76263ddeaaaa3016bc9da76ec18caddd95f99985
cm <itcaimeng@163.com>
2024-12-10 23:33:05 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios/index';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import store from './store'
 
+//设置Axios的基础url
+axios.defaults.baseURL = "http://localhost:33889/";
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+Vue.prototype.$axios=axios;
+
+Vue.use(ElementUI);
 
 console.log(store)
 
